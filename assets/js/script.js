26ccac9f3dbfab158cd016a45bb2e474360f2f2c
sthDINESH <sthapit.dnsh@gmail.com>
2025-08-07 16:44:34 +0100

    draw 2 cards each for dealer and player

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,21 +17,48 @@ const cardNumbers = [
   "King",
 ];
 const cardValues = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
-const cards = [];
+
+// Global object to store the cards available in the game
+const deck = [];
+
+// Global objects to store player and dealer details
+const dealer = {};
+const player = {};
 
 /**
- * Function to initialize global cards[] with the cards available in the game
+ * Function to initialize global deck[] with the deck available in the game
  *  */
-function initializeCards() {
+function initializeDeck() {
   for (let suit of suits) {
     for (let index = 0; index < cardValues.length; index++) {
-      cards.push({
+      deck.push({
         suit: suit,
         number: cardNumbers[index],
         value: cardValues[index],
       });
     }
   }
+}
+/**
+ * Draw deck from the deck and add it to user's hand
+ * @param {*} user: global object to add the drawn deck to
+ * @param {*} numCardsToDraw : number of deck to draw from the deck
+ */
+function drawCard(user, numCardsToDraw = 1) {
+  //   Create an empty array to store the hand if the key is not present
+  if (user["hand"] === undefined) {
+    user["hand"] = [];
+  }
+
+  for (let count = 0; count < numCardsToDraw; count++) {
+    // Pick a card at random index from the deck
+    let index = Math.floor(Math.random() * deck.length);
+    // Add the card to the user's hand
+    user["hand"].push(deck[index]);
+    // Remove the card from the deck
+    deck.splice(index, 1);
+  }
+//   user["sum"] = calculateSum(user["hand"]);
 }
 
 const btnsGame = document.querySelector("#btns-game");
@@ -48,7 +75,13 @@ const playGame = () => {
   btnsGame.appendChild(btnStand);
   btnsGame.removeChild(btnPlay);
 
-  //   Initialize available cards
-  initializeCards();
-  //   console.log(cards)
+  // Initialize available deck
+  initializeDeck();
+  console.log(deck);
+
+  // Initialize the dealer and player hand with 2 cards
+  drawCard(dealer,2);
+  drawCard(player,2);
+  console.log(dealer);
+  console.log(player);
 };
