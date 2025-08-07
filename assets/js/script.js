c8e8a91a2f9333a53c0c197e0868f83ac0eab47c
sthDINESH <sthapit.dnsh@gmail.com>
2025-08-07 16:18:51 +0100

    add constants for cards and call to init global array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,39 @@
+// Constants to represent a deck of cards
+const suits = ["Heart", "Spade", "Diamond", "Club"];
+// const suits = ["Heart"];
+const cardNumbers = [
+  "Ace",
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  "Jack",
+  "Queen",
+  "King",
+];
+const cardValues = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
+const cards = [];
+
+/**
+ * Function to initialize global cards[] with the cards available in the game
+ *  */
+function initializeCards() {
+  for (let suit of suits) {
+    for (let index = 0; index < cardValues.length; index++) {
+      cards.push({
+        suit: suit,
+        number: cardNumbers[index],
+        value: cardValues[index],
+      });
+    }
+  }
+}
+
 const btnsGame = document.querySelector("#btns-game");
 const btnPlay = document.querySelector("#btn-play");
 const btnHit = document.querySelector("#btn-hit");
@@ -11,4 +47,8 @@ const playGame = () => {
   btnsGame.appendChild(btnHit);
   btnsGame.appendChild(btnStand);
   btnsGame.removeChild(btnPlay);
+
+  //   Initialize available cards
+  initializeCards();
+  //   console.log(cards)
 };
